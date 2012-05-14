f9dee403d9f88e2463992a519171581e7d7dd598
Roman Dvornov <rdvornov@gmail.com>
2012-05-14 11:59:57 +0000

    rename fsobserver into server

--- tools/basis.cmd
+++ tools/basis.cmd
@@ -1,5 +1,5 @@
 @echo off
 set curpath=%cd%
 cd %0\..
-If "%1"=="server" node fsobserver\server.js %curpath% %2 
+If "%1"=="server" node server\server.js %curpath% %2 
 If "%1"=="rebuild" node build.js %2 %3
