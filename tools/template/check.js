49107a5f5bce0993baf485a729741ac89168ec8e
Roman Dvornov <rdvornov@gmail.com>
2012-06-03 19:33:24 +0000

    new simple tool for template test; now it test for predictable and show warnings on declaration create if any

---
+++ tools/template/check.js
@@ -0,0 +1,71 @@
+
+var path = require('path');
+var fs = require('fs');
+
+global.document = require('jsdom-nocontextifiy').jsdom();
+global.basis = require('../../src/basis.js').basis;
+basis.require('basis.template');
+
+var workDir = path.normalize(process.argv[2]);
+
+var flags = (function(){
+  function hasFlag(flag){
+    return args.indexOf(flag) != -1;
+  }
+
+  var args = process.argv.slice(2);
+  return {
+    showAll: hasFlag('-all')
+  };
+})();
+
+
+(function getTemplates(dir){
+  function readAndCheck(filename){
+    fs.readFile(filename, 'utf-8', function(err, content){
+      if (err)
+        return console.warn('readFile error:', err, filename);
+      
+      var decl = basis.template.makeDeclaration(content, path.dirname(filename) + '/', { debug: true });
+
+      if (flags.showAll || decl.unpredictable || decl.warns)
+      {
+        var code = 'OK';
+
+        if (decl.unpredictable)
+          code = 'UNPREDICTABLE';
+
+        if (decl.warns)
+          code = 'WARN';
+
+        console.log('[' + code + ']', path.normalize(filename));
+
+        if (decl.warns)
+          console.log('  [!] ' + decl.warns.join('\n  [!] ') + '\n');
+      }
+    });
+  }
+
+  function getStat(filename){
+    fs.stat(filename, function(err, stats){
+      if (err)
+        return console.warn('stat error:', err, filename);
+
+      if (stats.isDirectory())
+        getTemplates(filename);
+      else
+      {
+        if (path.extname(filename) == '.tmpl')
+          readAndCheck(filename);
+      }
+    })
+  }
+
+  fs.readdir(dir, function(err, files){
+    if (err)
+      return console.warn('readdir error:', err, dir);
+
+    for (var i = 0; i < files.length; i++)
+      getStat(dir + '/' + files[i]);
+  });
+})(workDir);
