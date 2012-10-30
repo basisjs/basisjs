d5ab153fe6e406fd2a508e95cba19d7307e30f09
lahmatiy <rdvornov@gmail.com>
2012-10-30 15:08:14 +0600

    warn on every cssClassName/id usage, not just on first one

--- src/basis/ui.js
+++ src/basis/ui.js
@@ -219,9 +219,6 @@
   var EMPTY_TEMPLATE = new Template('<div/>');
 
 
-  ;;;var oneTimeIdWarn = 0;
-  ;;;var oneTimeCssClassNameWarn = 0;
-
  /**
   * @mixin
   */
@@ -407,7 +404,7 @@
           // update template
           if (this.id)
           {
-            ;;;if (!oneTimeIdWarn++) basis.dev.warn('WARN: basis.ui.Node#id property is prohibited and being removed soon, class:', this.constructor.className, ', value:', this.id);
+            ;;;basis.dev.warn('WARN: basis.ui.Node#id property is prohibited and being removed soon, class:', this.constructor.className, ', value:', this.id);
 
             tmpl.element.id = this.id;
           }
@@ -415,7 +412,7 @@
           var cssClassNames = this.cssClassName;
           if (cssClassNames)
           {
-            ;;;if (!oneTimeCssClassNameWarn++) basis.dev.warn('WARN: basis.ui.Node#cssClassName property is prohibited and being removed soon, class:', this.constructor.className, ', value:', this.cssClassName);
+            ;;;basis.dev.warn('WARN: basis.ui.Node#cssClassName property is prohibited and being removed soon, class:', this.constructor.className, ', value:', this.cssClassName);
 
             if (typeof cssClassNames == 'string')
               cssClassNames = { element: cssClassNames };
