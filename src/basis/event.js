3b27836dd8dd25bee46bb43b0c9285fc5807ed0c
Roman Dvornov <rdvornov@gmail.com>
2012-11-27 03:06:06 +0600

    Make code more readable: move all debug code under `if (devmode)` condition

--- src/basis/event.js
+++ src/basis/event.js
@@ -18,64 +18,13 @@
   // Main part
   //
 
+  /** @const */ var DEVMODE = false /** @cut */ || true;
+
   var NULL_HANDLER = {};
   var events = {};
   var warnOnDestroy = function(){
     throw 'Object had been destroyed before. Destroy method must not be called more than once.';
   };
-
-
- /**
-  * Creates new type of event or returns existing one, if it was created before.
-  * @param {string} eventName
-  * @func
-  */
-  function createEvent(eventName){
-    var eventFunction = events[eventName];
-
-    if (!eventFunction)
-    {
-      eventFunction = events[eventName] = 
-        /** @cut for more verbose in dev */ new Function('eventName', 'slice', 'var eventFunction;\nreturn eventFunction = function _event_' + eventName + '(' + slice.call(arguments, 1).join(', ') + '){' +
-
-          function dispatchEvent(){
-            var args;
-            var fn;
-
-            if (eventFunction.listen)
-              if (fn = this.listen[eventFunction.listenName])
-                eventFunction.listen.call(this, fn, args = [this].concat(slice.call(arguments)));
-
-            var cursor = this;
-            while (cursor = cursor.handlers_)
-            {
-              // handler call
-              if (fn = cursor.handler[eventName])
-                if (typeof fn == 'function')
-                  fn.apply(cursor.context, args = args || [this].concat(slice.call(arguments)));
-
-              // any event handler
-              if (fn = cursor.handler['*'])
-                if (typeof fn == 'function')
-                  fn.call(cursor.context, {
-                    sender: this,
-                    type: eventName,
-                    args: arguments
-                  });
-            }
-
-            // WARN: this feature is not available in producation
-            ;;;if (this.event_debug) this.event_debug({ sender: this, type: eventName, args: arguments });
-          }
-
-        /** @cut for more verbose in dev */ .toString().replace(/\beventName\b/g, "'" + eventName + "'").replace(/^function[^(]*\(\)[^{]*\{|\}$/g, '') + '}')(eventName, slice);
-
-      if (LISTEN_MAP[eventName])
-        extend(eventFunction, LISTEN_MAP[eventName]);
-    }
-
-    return eventFunction;
-  }
 
 
   //
@@ -91,8 +40,8 @@
       LISTEN_MAP[eventName] = {
         listenName: listenName,
         listen: handler || function(listenHandler, args){
-          if (args[1])  // second argument is oldObject
-            args[1].removeHandler(listenHandler, this);
+          if (args[0])  // second argument is oldObject
+            args[0].removeHandler(listenHandler, this);
 
           if (this[propertyName])
             this[propertyName].addHandler(listenHandler, this);
@@ -107,8 +56,75 @@
 
 
  /**
-  * Base class for event dispacthing. It provides model when it's instance
-  * can registrate handlers for events, and call it when event happend. 
+  * Creates new type of event or returns existing one, if it was created before.
+  * @param {string} eventName
+  * @func
+  */
+  function createEvent(eventName){
+    var eventFunction = events[eventName];
+
+    if (!eventFunction)
+    {
+      eventFunction = function(){
+        var cursor = this;
+        var args;
+        var fn;
+
+        if (eventFunction.listen)
+          if (fn = this.listen[eventFunction.listenName])
+            eventFunction.listen.call(this, fn, arguments);
+
+        while (cursor = cursor.handlers_)
+        {
+          // handler call
+          if (fn = cursor.handler[eventName])
+            if (typeof fn == 'function')
+              fn.apply(cursor.context, args = args || [this].concat(slice.call(arguments)));
+
+          // any event handler
+          if (fn = cursor.handler['*'])
+            if (typeof fn == 'function')
+              fn.call(cursor.context, {
+                sender: this,
+                type: eventName,
+                args: arguments
+              });
+        }
+
+        // that feature available in development mode only
+        if (DEVMODE && this.event_debug)
+          this.event_debug({
+            sender: this,
+            type: eventName,
+            args: arguments
+          });
+      };
+
+      // function wrapper for more verbose in development mode
+      if (DEVMODE)
+      {
+        eventFunction = new Function('eventName', 'slice', 'DEVMODE',
+          'var eventFunction;\n' +
+          'return eventFunction = function _event_' + eventName + '(' + slice.call(arguments, 1).join(', ') + '){' +
+          eventFunction.toString()
+            .replace(/\beventName\b/g, "'" + eventName + "'")
+            .replace(/^function[^(]*\(\)[^{]*\{|\}$/g, '') + 
+        '}')(eventName, slice, DEVMODE);
+      }
+
+      events[eventName] = eventFunction;
+
+      if (LISTEN_MAP[eventName])
+        extend(eventFunction, LISTEN_MAP[eventName]);
+    }
+
+    return eventFunction;
+  }
+
+
+ /**
+  * Base class for event dispatching. It provides interface for instance
+  * to add and remove handler for desired events, and call it when event happens. 
   * @class
   */
   var Emitter = Class(null, {
@@ -116,23 +132,14 @@
 
    /**
     * List of event handler sets.
-    * @type {Array.<object>}
+    * @type {object}
     * @private
     */
     handlers_: null,
 
    /**
-    * Function that call on any event. Use it to debug purposes only.
-    * WARN: This functionality is not supported in producation.
-    * @type {function(event)}
-    * @debug
-    */
-    /** @cut */event_debug: null,
-
-   /**
     * Fires when object is destroing.
     * NOTE: don't override
-    * @param {basis.Emitter} object Reference for object wich is destroing.
     * @event
     */
     event_destroy: createEvent('destroy'),
@@ -142,7 +149,7 @@
     */
     listen: Class.nestedExtendProperty(),
 
-   /** use extend constructor */
+    // use extend constructor
     extendConstructor_: true,
 
    /**
@@ -159,17 +166,29 @@
     },
 
    /**
-    * Registrates new event handler set for object.
+    * Adds new event handler to object.
     * @param {object} handler Event handler set.
     * @param {object=} context Context object.
     */
     addHandler: function(handler, context){
-      ;;;if (!handler) basis.dev.warn('Emitter#addHandler: `handler` argument is not an object (', handler, ')');
+      if (DEVMODE && !handler)
+        basis.dev.warn('Emitter#addHandler: handler is not an object (', handler, ')');
 
       context = context || this;
       
       // warn about duplicates
-      ;;;var cursor = this; while (cursor = cursor.handlers_) if (cursor.handler === handler && cursor.context === context) { basis.dev.warn('Emitter#addHandler: Add duplicate handler', handler, ' to Emitter instance: ', this); break; }
+      if (DEVMODE)
+      {
+        var cursor = this;
+        while (cursor = cursor.handlers_)
+        {
+          if (cursor.handler === handler && cursor.context === context)
+          {
+            basis.dev.warn('Emitter#addHandler: add duplicate event handler', handler, 'to Emitter instance:', this);
+            break;
+          }
+        }
+      }
 
       // add handler
       this.handlers_ = { 
@@ -186,11 +205,7 @@
     * @param {object=} context Context object.
     */
     removeHandler: function(handler, context){
-      if (!this.handlers_)
-        return;
-
-      if (!context)
-        context = this;
+      context = context || this;
 
       // search for handler and remove it
       var cursor = this;
@@ -207,7 +222,8 @@
       }
 
       // handler not found
-      ;;;basis.dev.warn('Emitter#removeHandler: method didn\'t remove any handler');
+      if (DEVMODE && prev !== this)
+        basis.dev.warn('Emitter#removeHandler: nothing removed');
     },
 
    /**
@@ -215,18 +231,30 @@
     */
     destroy: function(){
       // warn on destroy method call (only in debug mode)
-      ;;;this.destroy = warnOnDestroy;
+      if (DEVMODE)
+        this.destroy = warnOnDestroy;
 
-      if (this.handlers_)
-      {
-        // fire object destroy event handlers
-        events.destroy.call(this);
+      // fire object destroy event handlers
+      this.event_destroy();
 
-        // remove all event handler sets
-        this.handlers_ = null;
-      }
+      // drop event handlers if any
+      this.handlers_ = null;
     }
   });
+
+
+  if (DEVMODE)
+  {
+    Emitter.extend({
+     /**
+      * Function that call on any event. Use it for debug purposes.
+      * WARN: This functionality is supported in development only.
+      * @type {function(event)}
+      */
+      event_debug: null
+    });
+  }
+
 
 
   //
