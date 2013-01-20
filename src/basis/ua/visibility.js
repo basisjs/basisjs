82ef0aebbf12c729e23bd84bb2d0793a99fa9077
Roman Dvornov <rdvornov@gmail.com>
2013-01-21 02:57:18 +0400

    refactoring

--- src/basis/ua/visibility.js
+++ src/basis/ua/visibility.js
@@ -1,4 +1,6 @@
 
+  basis.require('basis.event');
+
  /**
   * @namespace basis.ua.visibility
   */
@@ -10,91 +12,62 @@
   // Main part
   //
 
-  var visibilityPrefix;
-  var supported = false;
-  var inited = false;
-  var handlers = [];
+  var document = global.document;
 
 
   //
-  // Check visibility support and get prefix
+  // Check visibility support and resolve prefix and property name
   //
 
-  var prefixes = [ 'webkit', 'moz', 'o', 'ms' ]; 
-  if (document.visibilityState != undefined)
-    visibilityPrefix = '';
-  else
-  {
-    for (var i = 0, prefix; prefix = prefixes[i]; i++)
-      if (document[prefix + 'VisibilityState'])
-        visibilityPrefix = prefix;
-  }
+  var prefixes = ['webkit', 'moz', 'o', 'ms'];
+  var prefix = '';
+  var visibilityProperty = 'visibilityState';
 
-  supported = visibilityPrefix !== undefined;
+  while (!document[visibilityProperty])
+    if (visibilityProperty = prefix = prefixes.pop())
+      visibilityProperty += 'VisibilityState';
 
 
   //
-  // addHandler/removeHandler 
+  // visibility change emiiter
   //
 
-  function addHandler(handler, thisObject){
-    if (!supported)
-      return false;
-
-    addGlobalHandler();
-
-    for (var i = handlers.length, item; i-- > 0;)
-    {
-      item = handlers[i];
-      if (item.handler === handler && item.thisObject === thisObject)
-        return false;
-    }
-
-    return !!handlers.push({ 
-      handler: handler,
-      thisObject: thisObject
-    });
-
-  }
-
-  function removeHandler(handler, thisObject){
-    if (!supported)
-      return false;
-
-    for (var i = handlers.length, item; i-- > 0;)
-    {
-      item = handlers[i];
-      if (item.handler === handler && item.thisObject === thisObject)
-        return !!handlers.splice(i, 1);
-    }
-
-    return false;
-  }
-
-  function addGlobalHandler(){
-    document.addEventListener(visibilityPrefix + 'visibilitychange', onVisibilityChangedHandler, false);
-    addGlobalHandler = Function.$undef;
-  }
+  var listener = new basis.event.Emitter({
+    visible: basis.event.create('visible'),
+    hidden: basis.event.create('hidden')
+  });
 
   //
-  // global handler
-  // 
-  function onVisibilityChangedHandler(){
-    var visibilityState = getState();
-
-    for (var i = 0, handler; handler = handlers[i]; i++)
-    {
-      for (var j in handler.handler)
-      {
-        if (j == visibilityState)
-          handler.handler[j].call(handler.thisObject);
-      }
-    }
-  }
+  // visibility state getter
+  //
 
   function getState(){
-    return document[visibilityPrefix ? visibilityPrefix + 'VisibilityState' : 'visibilityState'] || 'visible';
+    return document[visibilityProperty] || 'visible';
   }
+
+
+  //
+  // add visibility change handler
+  //
+
+  var supported = visibilityProperty != null;
+  if (supported)
+    document.addEventListener(prefix + 'visibilitychange', function(){
+      //
+      // document.visibilityState
+      //
+      // Returns a string denoting the visibility state of the document. Possible values:
+      // * visible : the page content may be at least partially visible. In practice this means that the page is the foreground tab of a non-minimized window. 
+      // * hidden : the page content is not visible to the user. In practice this means that the document is either a background tab or part of a minimized window.
+      // * prerender : the page content is being prerendered and is not visible to the user (considered hidden for purposes of document.hidden).  The document may start in this state, but will never transition to it from another value.
+      //
+      // https://developer.mozilla.org/en-US/docs/DOM/Using_the_Page_Visibility_API#document.visibilityState
+ 
+      if (getState() == 'visible')
+        listener.visible();
+      else
+        listener.hidden();
+    }, false);
 
 
   //
@@ -102,7 +75,8 @@
   //
 
   module.exports = {
-    addHandler: addHandler,
-    removeHandler: removeHandler,
+    supported: supported,
+    addHandler: listener.addHandler.bind(listener),
+    removeHandler: listener.removeHandler.bind(listener),
     getState: getState
   };
