2f1a6598cb5d00f8aeac660551b459aab76eb19a
Vladimir Ratsev <wuzykk@gmail.com>
2013-02-04 19:16:45 +0600

    router refactoring, replace class with static functions

--- src/basis/router.js
+++ src/basis/router.js
@@ -32,122 +32,122 @@
     'i');
   }
 
+  var routes_ = {};
+  var timer = null;
+  var started_ = false;
+  var cachedPath_ = null;
 
-  //
-  // main part
-  //
 
-  var Router = basis.Class(null, {
-    routes_: null,
-    cachedPath_: null,
-    started: false,
-
-    init: function(){
-      this.routes_ = {};
-      this.cachedPath_ = '';
-    },
-    
-    add: function(path, callback, context){
-      var route = this.routes_[path]
-
-      if (!route)
-      {
-        route = {
-          source: path,
-          callbacks: [],
-          regexp: Object.prototype.toString.call(path) != '[object RegExp]'
-            ? pathToRegExp(path)
-            : path
-        };
-        this.routes_[path] = route;
-      }
-      
-      route.callbacks.push({
-        callback: callback,
-        context: context
-      });
-
-      if (this.cachedPath_)
-      {
-        var match = this.cachedPath_.match(route.regexp);
-        if (match)
-          callback.apply(context, basis.array.from(match, 1));
-      }
-    },
-    
-    navigate: function(path, replace){
-      if (replace)
-        location.replace(location.pathname + '#' + (path || '/'));
-      else
-        location.hash = path;
-
-      if (this.started)
-        this.checkUrl();
-    },
-
-    start: function(){
-      if (!this.started)
-      {
-        if (eventSupport)
-          basis.dom.event.addHandler(global, 'hashchange', this.checkUrl, this);
-        else
-          this.timer = setInterval(this.checkUrl.bind(this), 50);
-
-        ;;;basis.dev.log('Router stated');
-        this.started = true;
-      }
-
-      this.checkUrl();
-    },
-    
-    stop: function(){
+ /**
+  * Start router
+  */
+  function start(){
+    if (!started_)
+    {
       if (eventSupport)
-        basis.dom.event.removeHandler(global, 'hashchange', this.checkUrl, this);
+        basis.dom.event.addHandler(global, 'hashchange', checkUrl);
       else
-        clearInterval(this.timer);
+        timer_ = setInterval(checkUrl, 50);
 
       ;;;basis.dev.log('Router stated');
-      this.started = false;
-    },
+      started_ = true;
+    }
 
-    checkUrl: function(){
-      var curPath = location.hash.substr(1) || '/';
+    checkUrl();
+  }
 
-      if (curPath != this.cachedPath_)
+ /**
+  * Start router
+  */
+  function stop(){
+    if (eventSupport)
+      basis.dom.event.removeHandler(global, 'hashchange', checkUrl);
+    else
+      clearInterval(timer_);
+
+    ;;;basis.dev.log('Router stated');
+    started_ = false;
+  }
+
+ /**
+  * Process current location
+  */
+  function checkUrl(){
+    var curPath = location.hash.substr(1) || '/';
+
+    if (curPath != cachedPath_)
+    {
+      ;;;basis.dev.log('Router hash changed:', curPath);
+      cachedPath_ = curPath;
+
+      for (var path in routes_)
       {
-        ;;;basis.dev.log('Router hash changed:', curPath);
-        this.cachedPath_ = curPath;
-
-        for (var path in this.routes_)
+        var route = routes_[path];
+        var match = curPath.match(route.regexp);
+        if (match)
         {
-          var route = this.routes_[path];
-          var match = curPath.match(route.regexp);
-          if (match)
-          {
-            var args = basis.array.from(match, 1);
-            for (var i = 0, item; item = route.callbacks[i]; i++)
-              item.callback.apply(item.context, args);
+          var args = basis.array.from(match, 1);
+          for (var i = 0, item; item = route.callbacks[i]; i++)
+            item.callback.apply(item.context, args);
 
-            ;;;basis.dev.log('Router hash match:', route.source, args);
-          }
+          ;;;basis.dev.log('Router hash match:', route.source, args);
         }
       }
-    },
-
-    checkRoute: function(route, path){
-            
-    },
-    
-    destroy: function(){
-      this.stop();
     }
-  });
+  }
 
+ /**
+  * Add path to be handled
+  */
+  function add(path, callback, context){
+    var route = routes_[path];
+
+    if (!route)
+    {
+      route = {
+        source: path,
+        callbacks: [],
+        regexp: Object.prototype.toString.call(path) != '[object RegExp]'
+          ? pathToRegExp(path)
+          : path
+      };
+      routes_[path] = route;
+    }
+    
+    route.callbacks.push({
+      callback: callback,
+      context: context
+    });
+
+    if (cachedPath_)
+    {
+      var match = cachedPath_.match(route.regexp);
+      if (match)
+        callback.apply(context, basis.array.from(match, 1));
+    }
+  }
+
+ /**
+  * Navigate to specified path
+  */
+  function navigate(path, replace){
+    if (replace)
+      location.replace(location.pathname + '#' + (path || '/'));
+    else
+      location.hash = path;
+
+    if (started_)
+      checkUrl();
+  }
 
   //
   // export names
   //
 
   module.exports = {
-    Router: Router
+    add: add,
+    stop: stop,
+    start: start,
+    checkUrl: checkUrl,
+    navigate: navigate
   };
