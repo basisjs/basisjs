c4717f6a78f5173e4c191fb2ed7528d3f1358159
Roman Dvornov <rdvornov@gmail.com>
2013-02-06 03:03:06 +0400

    small fixes

--- demo/apps/todomvc/basis/js/app/type/todo.js
+++ demo/apps/todomvc/basis/js/app/type/todo.js
@@ -3,17 +3,17 @@
   basis.require('basis.data.dataset');
 
   var Todo = new basis.entity.EntityType({
-  	name: 'Todo',
-  	fields: {
-  		id: {
+    name: 'Todo',
+    fields: {
+      id: {
         type: basis.entity.IntId,
         calc: function(delta, data, oldValue){
           return data.id || ((lastId.value || 0) + 1);
         }
       },
-  		title: String,
-  		completed: Boolean
-  	}
+      title: String,
+      completed: Boolean
+    }
   });
 
   var lastId = basis.data.index.max(Todo.all, 'data.id');
