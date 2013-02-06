f4ac8abed12881f66f25b24e483daf544c287d5b
Roman Dvornov <rdvornov@gmail.com>
2013-02-06 04:10:19 +0400

    initial implementation of code launcher

--- tour/src/app.js
+++ tour/src/app.js
@@ -3,13 +3,36 @@
   basis.require('basis.ui');
 
 
+  var prevCode = '';
   module.exports = basis.app({
     init: function(){
       return new basis.ui.Node({
         template: resource('app/template/layout.tmpl'),
         binding: {
           toc: resource('module/toc/index.js').fetch()
+        },
+        action: {
+          runCode: function(event){
+            var value = event.sender.value;
+
+            try {
+              new Function(value);
+            } catch(e) {
+              return;
+            }
+
+            if (prevCode != value)
+            {
+              //prevCode = value;
+              this.tmpl.launcher.src = this.tmpl.launcher.src;
+            }
+          }
         }
       }).element;
     }
   });
+
+  global.launcherCallback = function(){
+    var sourceCodeNode = document.getElementById('code-editor');
+    return prevCode = sourceCodeNode ? sourceCodeNode.value : 'document.write("Source code not found")';
+  }
