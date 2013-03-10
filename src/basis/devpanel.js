375195bcf0eec1850abbe323493e612fe593edd6
Roman Dvornov <rdvornov@gmail.com>
2013-03-10 21:11:53 +0400

    getFileGraph command

--- src/basis/devpanel.js
+++ src/basis/devpanel.js
@@ -7,7 +7,17 @@ basis.ready(function(){
   transport.init();
 
   // prepare API object
-  basis.appCP = {};
+  basis.appCP = {
+    getFileGraph: function(){
+      if (basis.devtools)
+        basis.devtools.getFileGraph(function(err, data){
+          transport.sendData('fileGraph', {
+            err: err,
+            data: data
+          });
+        });
+    }
+  };
   basis.object.extend(basis.appCP, resource('devpanel/fileAPI.js').fetch());
   basis.object.extend(basis.appCP, resource('devpanel/l10nAPI.js').fetch());
   basis.object.extend(basis.appCP, resource('devpanel/templateAPI.js').fetch());  
