a5a2df624d00349b871b4a3343efe3cf5769f01d
Roman Dvornov <rdvornov@gmail.com>
2013-03-22 00:07:07 +0400

    fix slide code

--- tour/slide/ui_list/main.js
+++ tour/slide/ui_list/main.js
@@ -1,18 +1,23 @@
 basis.require('basis.ui');
 
+var Item = basis.ui.Node.subclass({
+  className: 'Item',
+  template:
+    '<li>{title}</li>',
+  binding: {
+    title: 'title'
+  }
+});
+
 var list = new basis.ui.Node({
   container: document.body,
-  template: resource('list.tmpl'),
-
-  childClass: {
-    template: resource('item.tmpl'),
-    binding: {
-      title: 'title'
-    }
-  },
+  template:
+    '<ul/>',
   childNodes: [
-    { title: 'Item foo' },
-    { title: 'Item bar' },
-    { title: 'Item baz' }
+    new Item({ title: 'foo' }),
+    new Item({ title: 'bar' })
   ]
 });
+
+list.appendChild(new Item({ title: 'I\'ll be last' }));
+list.insertBefore(new Item({ title: 'I\'ll be first' }), list.firstChild);
