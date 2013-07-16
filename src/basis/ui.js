cb08bd15535864d0c8a9c900b38a92cb07cfafd4
Roman Dvornov <rdvornov@gmail.com>
2013-07-16 23:27:48 +0400

    drop deprecated l10n: binding preset

--- src/basis/ui.js
+++ src/basis/ui.js
@@ -170,11 +170,6 @@
     };
   });
 
-  BINDING_PRESET.add('l10n', function(token){
-    ;;;basis.dev.warn('`l10n:` prefix in binding is deprecated. Use explicit basis.l10n.getToken("' + token + '") instead');
-    return basis.fn.$const(basis.l10n.getToken(token));
-  });
-
 
  /**
   * Base binding
