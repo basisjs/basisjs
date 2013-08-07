a919aea6b4d253c0c00dbadb337f88f8110e59f3
Roman Dvornov <rdvornov@gmail.com>
2013-08-08 03:12:25 +0400

    reworked to use basis.ui.ShadowNodeList

--- src/basis/ui/tabs.js
+++ src/basis/ui/tabs.js
@@ -181,25 +181,7 @@
 
     template: templates.TabSheet,
 
-    childClass: UINode,
-
-    templateSync: function(noRecreate){
-      var pageElement = this.tmpl.pageElement;
-      Tab.prototype.templateSync.call(this, noRecreate);
-      if (pageElement && this.tmpl.pageElement !== pageElement)
-      {
-        if (!this.tmpl.pageElement)
-          DOM.remove(pageElement);
-        else
-          DOM.replace(pageElement, this.tmpl.pageElement);
-      }
-    },
-
-    destroy: function(){
-      DOM.remove(this.tmpl.pageElement);
-      
-      Tab.prototype.destroy.call(this);
-    }
+    childClass: UINode
   });
 
 
@@ -213,30 +195,17 @@
 
     childClass: TabSheet,
 
-    insertBefore: function(newChild, refChild){
-      if (newChild = TabControl.prototype.insertBefore.call(this, newChild, refChild))
-      {
-        if (this.tmpl.pagesElement)
-          this.tmpl.pagesElement.insertBefore(newChild.tmpl.pageElement, newChild.nextSibling ? newChild.nextSibling.tmpl.pageElement : null);
-      }
-
-      return newChild;
-    },
-    removeChild: function(oldChild){
-    	if (oldChild = TabControl.prototype.removeChild.call(this, oldChild))
-      {
-        if (this.tmpl.pagesElement)
-          oldChild.element.appendChild(oldChild.tmpl.pageElement);
-      }
-      return oldChild;
-    },
-    clear: function(keepAlive){
-      // put pageElement back to TabSheet root element
-      this.childNodes.forEach(function(tabSheet){
-        tabSheet.element.appendChild(tabSheet.tmpl.pageElement);
-      });
-
-      TabControl.prototype.clear.call(this, keepAlive);
+    satelliteConfig: {
+      shadowPages: basis.ui.ShadowNodeList.subclass({
+        getChildNodesElement: function(host){
+          return host.tmpl.pagesElement;
+        },
+        childClass: {
+          getElement: function(node){
+            return node.tmpl.pageElement;
+          }
+        }
+      })
     }
   });
 
