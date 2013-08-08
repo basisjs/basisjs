27b4a6b92df561158b4636750cd63b861cf72533
Roman Dvornov <rdvornov@gmail.com>
2013-08-09 01:25:20 +0400

    remove basis.data.property namespace

--- src/basis/data/property.js
+++
@@ -1,6 +0,0 @@
-
-  basis.require('basis.data.value');
-
-  ;;;basis.dev.warn('Namespace `basis.data.property` is deprecated now, use `basis.data.value` namespace instead');
-
-  module.exports = basis.data.value.exports;
