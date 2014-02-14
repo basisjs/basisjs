47c21ad8c083588ae8a1b670c74d0a1b2919ffee
Roman Dvornov <rdvornov@gmail.com>
2014-02-14 05:33:07 +0400

    remove deprecated Box/Viewport/addBlockResizeHandler from basis.layout (added to patch)

--- src/basis/layout.js
+++ src/basis/layout.js
@@ -1,6 +1,5 @@
 
   basis.require('basis.dom.computedStyle');
-  basis.require('basis.dom');
 
 
  /**
@@ -16,11 +15,7 @@
 
   var document = global.document;
   var documentElement = document.documentElement;
-
-  var extend = basis.object.extend;
   var computedStyle = basis.dom.computedStyle.get;
-
-  var Class = basis.Class;
 
 
   //
@@ -144,128 +139,12 @@
 
 
   //
-  // Boxes
-  //
-
- /**
-  * @class
-  * @deprecated
-  */
-  var Box = Class(null, {
-    className: namespace + '.Box',
-
-    init: function(element, woCalc, relElement){
-      /** @cut */ basis.dev.warn('Class `basis.layout.Box` is deprecated now, use basis.layout.getBoundingRect function instead.');
-      this.reset();
-      this.element = element;
-      this.relElement = relElement;
-      if (!woCalc)
-        this.recalc(this.relElement);
-    },
-    reset: function(){
-      extend(this, {
-        top: NaN,
-        left: NaN,
-        bottom: NaN,
-        right: NaN,
-        width: NaN,
-        height: NaN,
-        defined: false
-      });
-    },
-    set: function(property, value){
-      if (this.defined)
-      {
-        switch (property.toLowerCase())
-        {
-          case 'left':   this.left   = value; this.right  = this.left  + this.width; break;
-          case 'right':  this.right  = value; this.left   = this.right - this.width; break;
-          case 'width':  this.width  = value; this.right  = this.left  + this.width; break;
-          case 'top':    this.top    = value; this.bottom = this.top    + this.height; break;
-          case 'bottom': this.bottom = value; this.top    = this.bottom - this.height; break;
-          case 'height': this.height = value; this.bottom = this.top    + this.height; break;
-        }
-
-        if (this.width <= 0 || this.height <= 0)
-          this.reset();
-      }
-
-      return this;
-    },
-    recalc: function(relElement){
-      this.reset();
-
-      var element = this.element;
-      if (element)
-      {
-        extend(this, getBoundingRect(element, relElement));
-        this.defined = true;
-      }
-
-      return this.defined;
-    },
-    intersect: function(box){
-      if (!this.defined)
-        return false;
-
-      if (box instanceof Box == false)
-        box = new Box(box);
-
-      return box.defined &&
-             box.right  > this.left &&
-             box.left   < this.right &&
-             box.bottom > this.top &&
-             box.top    < this.bottom;
-    },
-    destroy: function(){
-      this.element = null;
-      this.relElement = null;
-    }
-  });
-
-
- /**
-  * @class
-  * @deprecated
-  */
-  var Viewport = Class(Box, {
-    className: namespace + '.Viewport',
-
-    init: function(){
-       /** @cut */ basis.dev.warn('Class `basis.layout.Viewport` is deprecated now, use basis.layout.getBoundingRect function instead.');
-       Box.prototype.init.call(this);
-    },
-
-    recalc: function(){
-      this.reset();
-
-      var element = this.element;
-      if (element)
-      {
-        extend(this, getViewportRect(element));
-        this.defined = true;
-      }
-
-      return this.defined;
-    }
-  });
-
-
-  //
   // export names
   //
 
   module.exports = {
-    Box: Box,
-    Viewport: Viewport,
-
     getOffsetParent: getOffsetParent,
     getTopLeftPoint: getTopLeftPoint,
     getBoundingRect: getBoundingRect,
-    getViewportRect: getViewportRect,
-
-    addBlockResizeHandler: function(element, fn){
-      /** @cut */ basis.dev.warn('`basis.layout.addBlockResizeHandler` is deprecated now, use basis.dom.resize.add/basis.dom.resize.remove functions instead.');
-      listenResize(element, fn);
-    }
+    getViewportRect: getViewportRect
   };
