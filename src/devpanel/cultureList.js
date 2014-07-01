c5118917b0d3d4f41e2918420f213e0d66d3300f
Roman Dvornov <rdvornov@gmail.com>
2014-07-01 17:31:36 +0400

    devpanel: remove flags from culture list

--- src/devpanel/cultureList.js
+++ src/devpanel/cultureList.js
@@ -6,24 +6,11 @@ var Menu = require('./module/menu/index.js');
 
 module.exports = new Menu({
   childClass: {
-    template: resource('./template/cultureItem.tmpl'),
     binding: {
       title: 'value',
       selected: basis.data.Value.from(inspectBasisL10n.culture).compute(function(node, value){
         return node.value == value;
-      }),
-      spriteX: {
-        events: 'update',
-        getter: function(node){
-          return node.country ? 16 * (node.country.charCodeAt(0) - 65) : 1000;
-        }
-      },
-      spriteY: {
-        events: 'update',
-        getter: function(node){
-          return node.country ? 11 * (node.country.charCodeAt(1) - 65) : 1000;
-        }
-      }
+      })
     },
     action: {
       select: function(){
