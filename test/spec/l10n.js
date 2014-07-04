ade9411a8724d7c3720b31d840081d8e366a27cc
Roman Dvornov <rdvornov@gmail.com>
2014-07-04 19:30:32 +0400

    test: clean up l10n test

--- test/spec/l10n.js
+++ test/spec/l10n.js
@@ -10,69 +10,105 @@ module.exports = {
   test: [
     {
       name: 'fallback',
-      test: function(){
-        var res = JSON.parse(basis.require('./l10n.l10n').resource.get(true));
-        var dict = basis.l10n.dictionary('./l10n.l10n');
-        basis.l10n.setCulture('en-US');
-        assert(dict.token('value').value === 'base');
+      test: [
+        {
+          name: 'basic',
+          test: function(){
+            var res = JSON.parse(basis.require('./l10n.l10n').resource.get(true));
+            var dict = basis.l10n.dictionary('l10n', basis.resource.virtual('l10n', res));
+            basis.l10n.setCulture('en-US');
+            assert(dict.token('value').value === 'base');
 
-        basis.l10n.setCultureList('en-US a/b b/c c');
+            basis.l10n.setCultureList('en-US a/b b/c c');
 
-        basis.l10n.setCulture('c');
-        assert(dict.token('value').value === 'base');
-        basis.l10n.setCulture('b');
-        assert(dict.token('value').value === 'base');
-        basis.l10n.setCulture('a');
-        assert(dict.token('value').value === 'base');
+            basis.l10n.setCulture('c');
+            assert(dict.token('value').value === 'base');
+            basis.l10n.setCulture('b');
+            assert(dict.token('value').value === 'base');
+            basis.l10n.setCulture('a');
+            assert(dict.token('value').value === 'base');
 
-        res.c = { value: 'c' };
-        dict.resource.update(JSON.stringify(res));
-        basis.l10n.setCulture('c');
-        assert(dict.token('value').value === 'c');
+            res.c = { value: 'c' };
+            dict.resource.update(JSON.stringify(res));
+            basis.l10n.setCulture('c');
+            assert(dict.token('value').value === 'c');
 
-        basis.l10n.setCulture('b');
-        assert(dict.token('value').value === 'c');
-        basis.l10n.setCulture('a');
-        assert(dict.token('value').value === 'c');
+            basis.l10n.setCulture('b');
+            assert(dict.token('value').value === 'c');
+            basis.l10n.setCulture('a');
+            assert(dict.token('value').value === 'c');
 
-        res.b = { value: 'b' };
-        dict.resource.update(JSON.stringify(res));
-        assert(dict.token('value').value === 'b');
-        basis.l10n.setCulture('b');
-        assert(dict.token('value').value === 'b');
-        basis.l10n.setCulture('c');
-        assert(dict.token('value').value === 'c');
+            res.b = { value: 'b' };
+            dict.resource.update(JSON.stringify(res));
+            assert(dict.token('value').value === 'b');
+            basis.l10n.setCulture('b');
+            assert(dict.token('value').value === 'b');
+            basis.l10n.setCulture('c');
+            assert(dict.token('value').value === 'c');
 
-        res.a = { value: 'a' };
-        dict.resource.update(JSON.stringify(res));
-        basis.l10n.setCulture('a');
-        assert(dict.token('value').value === 'a');
-        basis.l10n.setCulture('b');
-        assert(dict.token('value').value === 'b');
-        basis.l10n.setCulture('c');
-        assert(dict.token('value').value === 'c');
-        
-        res.b = { 
-          token: {
-            key1: 'a',
-            key2: 'b'
+            res.a = { value: 'a' };
+            dict.resource.update(JSON.stringify(res));
+            basis.l10n.setCulture('a');
+            assert(dict.token('value').value === 'a');
+            basis.l10n.setCulture('b');
+            assert(dict.token('value').value === 'b');
+            basis.l10n.setCulture('c');
+            assert(dict.token('value').value === 'c');
           }
-        };
-        dict.resource.update(JSON.stringify(res));
-        basis.l10n.setCulture('a');
-        var node = new basis.ui.Node({ template: basis.resource('./l10n.tmpl') });
-        node.tmpl.set('key', 'key1');
-        assert(node.element.innerText === 'a');
-        res.a = { 
-          token: {
-            key1: 'c'
+        },
+        {
+          name: 'fallback and compute token',
+          test: function(){
+            basis.l10n.setCultureList('a/b b');
+
+            var dict = basis.l10n.dictionary(basis.resource.virtual('l10n', {
+              a: {
+                token: {
+                  key1: 'a'
+                }
+              },
+              b: {
+                token: {
+                  key1: 'b1',
+                  key2: 'b2'
+                }
+              }
+            }));
+
+            var checkToken = dict.token('token').computeToken();
+
+            // base culture A
+            checkToken.set('key1');
+            assert(basis.l10n.getCulture() === 'a');
+            assert(checkToken.get() === 'a');
+
+            checkToken.set('key2');
+            assert(checkToken.get() === 'b2');
+
+            // set culture B
+            basis.l10n.setCulture('b');
+            assert(basis.l10n.getCulture() === 'b');
+            assert(checkToken.get() === 'b2');
+
+            checkToken.set('key1');
+            assert(checkToken.get() === 'b1');
+
+            checkToken.set('key2');
+            assert(checkToken.get() === 'b2');
+
+            // set culture A
+            basis.l10n.setCulture('a');
+            assert(basis.l10n.getCulture() === 'a');
+            assert(checkToken.get() === 'b2');
+
+            checkToken.set('key1');
+            assert(checkToken.get() === 'a');
+
+            checkToken.set('key2');
+            assert(checkToken.get() === 'b2');
           }
-        };        
-        dict.resource.update(JSON.stringify(res));
-        assert(node.element.innerText === 'c');
-        node.tmpl.set('key', 'key2');
-        assert(node.element.innerText === 'b');
-      }
+        }
+      ]
     },
     {
       name: 'dictionary',
@@ -80,12 +116,25 @@ module.exports = {
         assert(basis.l10n.dictionary('./l10n.l10n') === basis.l10n.dictionary('./l10n.l10n'));
         assert(basis.l10n.dictionary(basis.resource('./l10n.l10n')) === basis.l10n.dictionary('./l10n.l10n'));
 
+        // path should be normalized
+        assert(basis.l10n.dictionary('./foo/../l10n.l10n') === basis.l10n.dictionary('./l10n.l10n'));
+        // file extension should be replaced for `.l10n`
+        assert(basis.l10n.dictionary('./l10n.l10n') === basis.l10n.dictionary('./l10n.whatever'));
+        // if no extension, then `.l10n` should be appended
+        assert(basis.l10n.dictionary('./l10n.l10n') === basis.l10n.dictionary('./l10n'));
+      }
+    },
+    {
+      name: 'dictionary from static data (experimental, under consideration)',
+      test: function(){
         var staticdata = { 'ru-RU': { test: 'Test' } };
         var dict = basis.l10n.dictionary(staticdata);
         assert(dict instanceof basis.l10n.Dictionary);
+
+        // static data should produce the same dictionary (under consideration)
         assert(basis.l10n.dictionary(staticdata) !== basis.l10n.dictionary(staticdata));
 
-        //
+        // check dictionary
         basis.l10n.setCultureList('en-US ru-RU');
 
         basis.l10n.setCulture('en-US');
