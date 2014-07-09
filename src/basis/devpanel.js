c9e526b4bb16dca503f1af900b40cd16f9c90e99
Roman Dvornov <rdvornov@gmail.com>
2014-07-09 18:38:28 +0400

    devpanel: do nothing in build mode

--- src/basis/devpanel.js
+++ src/basis/devpanel.js
@@ -1,13 +1,15 @@
-// create separate instance of basis.js to avoid influence on original one
-basis.createSandbox({
-  inspect: basis,
-  modules: {
-    devpanel: {
-      autoload: true,
-      filename: basis.path.dirname(basis.filename_) + '/devpanel/index.js'
-    }
-  }
-});
+// do nothing in build mode
+/** @cut */ if (!basis.filename_)
+/** @cut */   return;
 
-// croak namespace
-delete basis.devpanel;
+// create separate instance of basis.js core to avoid influence on original one
+// do it only in dev mode
+/** @cut */ basis.createSandbox({
+/** @cut */   inspect: basis,
+/** @cut */   modules: {
+/** @cut */     devpanel: {
+/** @cut */       autoload: true,
+/** @cut */       filename: basis.path.dirname(basis.filename_) + '/devpanel/index.js'
+/** @cut */     }
+/** @cut */   }
+/** @cut */ });
