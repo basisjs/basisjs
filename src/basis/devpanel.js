1156d84617f96def612ed474108039c8dfa8baeb
Roman Dvornov <rdvornov@gmail.com>
2014-07-10 17:44:50 +0400

    devpanel: fix build issue in non-pack mode

--- src/basis/devpanel.js
+++ src/basis/devpanel.js
@@ -1,15 +1,15 @@
-// do nothing in build mode
-/** @cut */ if (!basis.filename_)
-/** @cut */   return;
-
-// create separate instance of basis.js core to avoid influence on original one
-// do it only in dev mode
-/** @cut */ basis.createSandbox({
-/** @cut */   inspect: basis,
-/** @cut */   modules: {
-/** @cut */     devpanel: {
-/** @cut */       autoload: true,
-/** @cut */       filename: basis.path.dirname(basis.filename_) + '/devpanel/index.js'
+/** @cut */ // do nothing in build mode
+/** @cut */ if (basis.filename_)
+/** @cut */ {
+/** @cut */   // create separate instance of basis.js core to avoid influence on original one
+/** @cut */   // do it only in dev mode
+/** @cut */   basis.createSandbox({
+/** @cut */     inspect: basis,
+/** @cut */     modules: {
+/** @cut */       devpanel: {
+/** @cut */         autoload: true,
+/** @cut */         filename: basis.path.dirname(basis.filename_) + '/devpanel/index.js'
+/** @cut */       }
 /** @cut */     }
-/** @cut */   }
-/** @cut */ });
+/** @cut */   });
+/** @cut */ }
