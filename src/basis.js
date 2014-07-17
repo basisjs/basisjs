a452f9b2a0aa73d4b74f22aab9b17475efab2fb2
Roman Dvornov <rdvornov@gmail.com>
2014-07-17 18:34:01 +0400

    core: small improvement in genUID()

--- src/basis.js
+++ src/basis.js
@@ -52,14 +52,18 @@
   * @returns {string} Generated id.
   */
   function genUID(len){
+    function base36(val){
+      return Math.round(val).toString(36);
+    }
+
     // uid should starts with alpha
-    var result = String.fromCharCode(97 + Math.round(25 * Math.random()));
+    var result = base36(10 + 25 * Math.random());
 
     if (!len)
       len = 16;
 
     while (result.length < len)
-      result += parseInt(new Date * Math.random(), 10).toString(36);
+      result += base36(new Date * Math.random());
 
     return result.substr(0, len);
   }
