c76ce23d7f96cfbfcfdea1b0d92ca3cc9bec08e7
Roman Dvornov <rdvornov@gmail.com>
2014-07-29 00:48:51 +0400

    devpanel: warn on require('basis.devnapel') to use new api

---
+++ src/basis/devpanel.js
@@ -0,0 +1,5 @@
+/** @cut */ if (basis.initDevpanel)
+/** @cut */ {
+/** @cut */   basis.dev.warn('`basis.require(\'basis.devpanel\')` is deprecated, use `basis.initDevpanel()` or emit `basisjs:init-devpanel` event on document instead.');
+/** @cut */   basis.initDevpanel();
+/** @cut */ }
