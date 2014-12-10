dcccc06001c4165e5fa47eecbf3fbcb40c945193
Roman Dvornov <rdvornov@gmail.com>
2014-12-11 00:51:24 +0300

    devpanel: bindings for regular attributes in template viewer

--- src/devpanel/inspector/template-info/build-tree.js
+++ src/devpanel/inspector/template-info/build-tree.js
@@ -40,6 +40,7 @@ var Element = DOMNode.subclass({
   binding: {
     name: 'name',
     childrenHidden: 'childrenHidden',
+    inline: 'inlineChildren',
     attributes: 'satellite:',
     singleton: function(node){
       return SINGLETON.indexOf(node.name) != -1;
@@ -76,6 +77,83 @@ var Comment = DOMNode.subclass({
   }
 });
 
+function buildAttribute(attr, attrBindings){
+  var value = {
+    type: 'static',
+    value: attr.value
+  };
+
+  switch (attr.name)
+  {
+    case 'class':
+      var bindingMap = {};
+
+      for (var i = 0, bind; bind = attrBindings[i]; i++)
+        if (bind.val)
+          bindingMap[bind.val] = bind;
+
+      value = attr.value.split(/(\s+)/).map(function(part){
+        var bind = bindingMap.hasOwnProperty(part) ? bindingMap[part] : null;
+        return {
+          type: bind ? 'binding' : 'static',
+          value: part,
+          bindingName: bind && bind.binding
+        };
+      });
+
+      break;
+
+    case 'style':
+      // todo
+      break;
+
+    default:
+      if (attr.value)
+        if (/^event-/.test(attr.name))
+          value.type = 'action';
+        else
+        {
+          if (attrBindings.length)
+          {
+            var baseBinding = attrBindings[0];
+            var bindingValues = {};
+
+            // build value map
+            for (var i = 0, bind; bind = attrBindings[i]; i++)
+              if (bind.binding)
+                bindingValues[bind.binding] = bind.raw;
+
+            if (baseBinding.type == 'bool')
+              value = {
+                type: 'binding',
+                value: baseBinding.val,
+                bindingName: attrBindings.length > 1 ? 'multiple' : baseBinding.binding
+              };
+            else
+              // convert expression to value parts
+              value = baseBinding.expr[0].map(function(item){
+                if (typeof item == 'number')
+                  return {
+                    type: 'binding',
+                    value: String(bindingValues[this[item]]),
+                    bindingName: this[item]
+                  };
+
+                return {
+                  type: 'static',
+                  value: item
+                };
+              }, attrBindings[0].expr[1]);
+          }
+        }
+  }
+
+  return {
+    name: attr.name,
+    childNodes: value
+  };
+}
+
 module.exports = function buildNode(item, bindings){
   function findBinding(node){
     return basis.array.search(bindings, node, 'dom');
@@ -91,51 +169,28 @@ module.exports = function buildNode(item, bindings){
   switch (node.nodeType)
   {
     case 1:
-      var attrs = Array.prototype.slice.call(node.attributes).map(function(attr){
-        var value;
-
-        if (attr.name == 'class')
-        {
-          var bindingMap = {};
-          for (var i = 0; i < bindings.length; i++)
-          {
-            var bind = bindings[i];
-            var val = bind.dom === node && bind.attr === attr.name && bind.val;
-            if (val)
-              bindingMap[val] = bind;
-          }
-
-          value = attr.value.split(/(\s+)/).map(function(part){
-            var bind = bindingMap.hasOwnProperty(part) ? bindingMap[part] : null;
-            return {
-              type: bind ? 'binding' : 'static',
-              value: part,
-              bindingName: bind && bind.binding
-            };
-          });
-        }
-        else
-        {
-          if (attr.value)
-            value = [{
-              type: /^event-/.test(attr.name) ? 'action' : 'static',
-              value: attr.value
-            }];
-        }
-
-        return {
-          name: attr.name,
-          childNodes: value
-        };
+      var attrs = basis.array(node.attributes).map(function(attr){
+        return buildAttribute(attr, bindings.filter(function(bind){
+          return bind.dom === node && bind.attr === attr.name;
+        }));
       });
+      var children = item[1].map(function(child){
+        return buildNode(child, bindings);
+      });
+      var inline =
+        children.every(function(node){
+          return node instanceof Text;
+        }) &&
+        children.reduce(function(res, node){
+          return res + node.value.length;
+        }, 0) < 32;
 
       return new Element({
         name: node.tagName.toLowerCase(),
         childrenHidden: node.firstChild && !item[1].length,
+        inlineChildren: inline,
         attributes: attrs,
-        childNodes: item[1].map(function(child){
-          return buildNode(child, bindings);
-        })
+        childNodes: children
       });
 
       break;
