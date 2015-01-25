56f81b2ae166ca245059dec7c6adfe6d996a29fd
konstantin <krivlenia@targetprocess.com>
2015-01-25 17:50:25 +0300

    add travis conf

--- karma.conf.js
+++ karma.conf.js
@@ -27,14 +27,13 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+    //preprocessors: {"src/**/*.js": "coverage"},
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: ['dots'/*,'coverage'*/],
 
 
     // web server port
@@ -56,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
