3c3c02a3c322e032e55f7ee2e2f181cac031edc3
tyanas <tyanas.go@gmail.com>
2016-01-18 20:09:31 +0300

    master fix default naespaceURI

--- test/spec/template/namespaces.js
+++ test/spec/template/namespaces.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'namespaces',
   init: function(){
     var namespaceURI = {
-      html: 'http://www.w3.org/1999/xhtml',
+      html: document.documentElement.namespaceURI,
       xmlns: 'http://www.w3.org/2000/xmlns',
       xlink: 'http://www.w3.org/1999/xlink',
       svg: 'http://www.w3.org/2000/svg'
