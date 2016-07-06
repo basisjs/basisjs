151c96602505bdb960b11cba8ea6159a64d6cfaf
Roman Dvornov <rdvornov@gmail.com>
2016-07-07 02:20:51 +0300

    tour: change file layout

--- tour/src/app.js
+++ tour/src/app.js
@@ -2,7 +2,7 @@ var l10n = require('basis.l10n');
 var router = require('basis.router');
 var Node = require('basis.ui').Node;
 var Value = require('basis.data').Value;
-var Slide = require('app.type').Slide;
+var Slide = require('./type/index.js').Slide;
 
 // temporary here
 l10n.setCultureList('en-US/ru-RU ru-RU');
@@ -17,7 +17,7 @@ var selectedSlide = router.route('*slide').param(0).as(function(slide){
 module.exports = require('basis.app').create({
   init: function(){
     return new Node({
-      template: resource('./app/template/layout.tmpl'),
+      template: resource('./template/layout.tmpl'),
       binding: {
         content: 'satellite:'
       },
---
+++ tour/src/type/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  File: require('./file.js'),
+  Slide: require('./slide.js')
+};
+
+require('basis.entity').validate();
