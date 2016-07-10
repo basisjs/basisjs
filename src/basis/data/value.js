5b3fcf8569233b6e903721a87bf55da907b9be3a
Roman Dvornov <rdvornov@gmail.com>
2016-07-10 19:04:41 +0300

    drop basis.data.value.Property

--- src/basis/data/value.js
+++ src/basis/data/value.js
@@ -1,59 +1,15 @@
+// import names
 
- /**
-  * Namespace overview:
-  * - {basis.data.value.Property}
-  * - {basis.data.value.ObjectSet}
-  * - {basis.data.value.Expression}
-  *
-  * @see ./demo/data/basic.html
-  *
-  * @namespace basis.data.value
-  */
-
-  var namespace = 'basis.data.value';
+var ObjectSet = require('./ObjectSet.js');
+var Expression = require('./Expression.js');
 
 
-  // import names
+//
+// export names
+//
 
-  var basisData = require('basis.data');
-  var Value = basisData.Value;
-  var ObjectSet = require('./ObjectSet.js');
-  var Expression = require('./Expression.js');
-
-
- /**
-  * @class
-  */
-  var Property = Value.subclass({
-    className: namespace + '.Property',
-
-    // use custom constructor
-    extendConstructor_: false,
-
-   /**
-    * @param {object} initValue Initial value for object.
-    * @param {object=} handler
-    * @param {function()=} proxy
-    * @constructor
-    */
-    init: function(initValue, handler, proxy){
-      this.value = initValue;
-      this.handler = handler;
-      this.proxy = proxy;
-
-      Value.prototype.init.call(this);
-    }
-  });
-
-
-
-  //
-  // export names
-  //
-
-  module.exports = {
-    Property: Property,
-    ObjectSet: ObjectSet,
-    Expression: Expression,
-    expression: Expression.create
-  };
+module.exports = {
+  ObjectSet: ObjectSet,
+  Expression: Expression,
+  expression: Expression.create
+};
