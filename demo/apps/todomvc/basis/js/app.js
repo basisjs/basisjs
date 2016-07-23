c11aaaa4a9f1390ecf3d6d45d0fdecf537df25e5
Roman Dvornov <rdvornov@gmail.com>
2016-07-23 23:20:58 +0300

    small fixes in TodoMVC demo

--- demo/apps/todomvc/basis/js/app.js
+++ demo/apps/todomvc/basis/js/app.js
@@ -2,39 +2,17 @@ var router = require('basis.router');
 var Node = require('basis.ui').Node;
 var Todo = require('app.type').Todo;
 
-
-//
 // Create app instance
-//
-
-module.exports = require('basis.app').create({
+require('basis.app').create({
   // replace container with id="todoapp" by app root node element
   replace: 'todoapp',
 
   // init method invoke on document ready
   init: function(){
     // set up router
-    router.add(/^\/(active|completed)$/, {
-      // Triggered every time when path starts to match the rule on change.
-      // Analogy with onmouseenter
-      // No need to define it by default.
-      enter: function(){
-      },
-
-      // Triggered every time when path changes and path matches the rule.
-      match: function(kind){
-        Todo.selected.set(Todo[kind]);
-      },
-
-      // Triggered every time when path stops to match the rule on change.
-      // Analogy with onmouseleave
-      leave: function(){
-        Todo.selected.set(Todo.all);
-      }
+    router.route(/^\/(active|completed)$/).param(0).as(function(subset){
+      Todo.selected.set(Todo[subset || 'all']);
     });
-
-    // start router
-    router.start();
 
     // return app root node
     return new Node({
