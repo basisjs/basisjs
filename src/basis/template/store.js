0094acbffd13b5c0d00931669d07c5fa3872363a
Roman Dvornov <rdvornov@gmail.com>
2016-07-27 17:09:01 +0300

    template: simplify access to template instance description
    - implement basis.template.resolveInfoByNode()
    - implement basis.template.resolveInfoById()

--- src/basis/template/store.js
+++ src/basis/template/store.js
@@ -22,46 +22,62 @@ function remove(id){
 // resolve functions
 //
 
-function resolveTemplateById(refId){
-  var templateId = refId & 0xFFF;
-  var object = templates[templateId];
-
-  return object && object.template;
-}
-
 function resolveInstanceById(refId){
   var templateId = refId & 0xFFF;
   var instanceId = refId >> 12;
-  var object = templates[templateId];
+  var templateInfo = templates[templateId];
 
-  return object && object.instances[instanceId];
+  return templateInfo && templateInfo.instances[instanceId];
+}
+
+function resolveInfoById(refId){
+  var templateId = refId & 0xFFF;
+  var instanceId = refId >> 12;
+  var templateInfo = templates[templateId];
+  var instanceInfo = templateInfo && templateInfo.instances[instanceId];
+
+  if (instanceInfo)
+    return {
+      /** @cut */ debug: instanceInfo.debug ? instanceInfo.debug() : null,
+      id: refId,
+      template: templateInfo.template,
+      context: instanceInfo.context,
+      tmpl: instanceInfo.tmpl
+    };
+}
+
+function resolveTemplateById(refId){
+  var templateId = refId & 0xFFF;
+  var templateInfo = templates[templateId];
+
+  return templateInfo && templateInfo.template;
 }
 
 function resolveObjectById(refId){
-  var templateRef = resolveInstanceById(refId);
+  var instanceInfo = resolveInstanceById(refId);
 
-  return templateRef && templateRef.context;
+  return instanceInfo && instanceInfo.context;
 }
 
 function resolveTmplById(refId){
-  var templateRef = resolveInstanceById(refId);
+  var instanceInfo = resolveInstanceById(refId);
 
-  return templateRef && templateRef.tmpl;
+  return instanceInfo && instanceInfo.tmpl;
 }
 
 function resolveActionById(refId){
-  var templateRef = resolveInstanceById(refId);
+  var instanceInfo = resolveInstanceById(refId);
 
-  return templateRef && {
-    context: templateRef.context,
-    action: templateRef.action
+  return instanceInfo && {
+    context: instanceInfo.context,
+    action: instanceInfo.action
   };
 }
 
 /** @cut */ function getDebugInfoById(refId){
-/** @cut */   var templateRef = resolveInstanceById(refId);
+/** @cut */   var instanceInfo = resolveInstanceById(refId);
 /** @cut */
-/** @cut */   return templateRef && templateRef.debug && templateRef.debug();
+/** @cut */   return instanceInfo && instanceInfo.debug && instanceInfo.debug();
 /** @cut */ }
 
 
@@ -71,8 +87,9 @@ module.exports = {
   add: add,
   remove: remove,
 
-  resolveActionById: resolveActionById,
+  resolveInfoById: resolveInfoById,
   resolveTemplateById: resolveTemplateById,
   resolveObjectById: resolveObjectById,
-  resolveTmplById: resolveTmplById
+  resolveTmplById: resolveTmplById,
+  resolveActionById: resolveActionById
 };
