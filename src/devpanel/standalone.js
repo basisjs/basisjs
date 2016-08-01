0fa1d175ecc8dc389be930b2f1e35481665cdd51
Roman Dvornov <rdvornov@gmail.com>
2016-08-01 21:33:38 +0300

    devpanel: unsubscribe from socket

--- src/devpanel/standalone.js
+++ src/devpanel/standalone.js
@@ -2,17 +2,25 @@ var View = require('./inspector/template-info/view/index.js');
 var createRemoteApi = require('./inspector/template-info/createRemoteApi.js');
 
 basis.ready(function(){
-  setTimeout(function(){
-    socket.on('basisjs.devpanel.data', function(data){
-      if (data.type === 'template')
-        view.set(data.payload);
-    });
+  function sync(data){
+    if (data.type === 'template')
+      view.set(data.payload);
+  }
 
-    var view = new View({
+  var view;
+
+  setTimeout(function(){
+    socket.on('basisjs.devpanel.data', sync);
+    view = new View({
       modal: false,
       mode: 'standalone',
       api: createRemoteApi(socket),
       zIndex: 1000
     });
+
   }, 200);
+
+  basis.teardown(function(){
+    socket.off('basisjs.devpanel.data', sync);
+  });
 });
