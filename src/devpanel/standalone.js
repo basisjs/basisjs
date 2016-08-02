d4482fb54001368dcd68ab9937e6c267e880fe15
Roman Dvornov <rdvornov@gmail.com>
2016-08-02 04:59:11 +0300

    devpanel: fix style and output message when nothing selected for standalone

--- src/devpanel/standalone.js
+++ src/devpanel/standalone.js
@@ -1,3 +1,5 @@
+var Value = require('basis.data').Value;
+var Node = require('basis.ui').Node;
 var View = require('./inspector/template-info/view/index.js');
 var createRemoteApi = require('./inspector/template-info/createRemoteApi.js');
 
@@ -18,6 +20,14 @@ basis.ready(function(){
       zIndex: 1000
     });
 
+    // temporary solution
+    new Node({
+      container: document.body,
+      template: '<div b:show="{show}" style="color: #AAA; text-align: center; font: 20px Arial; padding: 40px;">Component is not selected</div>',
+      binding: {
+        show: Value.query(view, 'visible').as(basis.bool.invert)
+      }
+    });
   }, 200);
 
   basis.teardown(function(){
