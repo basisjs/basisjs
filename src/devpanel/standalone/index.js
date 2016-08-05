6bbb1513bcdb00b07697cf045373123b44882f5b
Roman Dvornov <rdvornov@gmail.com>
2016-08-05 20:07:42 +0300

    devpanel: init devtool api on standalone client side

--- src/devpanel/standalone/index.js
+++ src/devpanel/standalone/index.js
@@ -1,11 +1,16 @@
 var Node = require('basis.ui').Node;
 var View = require('../view/template-info/view/index.js');
+var initDevtoolApi = location.hash.substr(1);
+var devtool = typeof top[initDevtoolApi] === 'function' ? top[initDevtoolApi]() : null;
+
+if (!devtool)
+  throw new Error('Devtool init handler is missed (should be present in location hash)');
 
 require('basis.template').setTheme('standalone');
 require('../api/file_.js');
 require('api').remote(
-  global.devtoolApi.send,
-  global.devtoolApi.subscribe
+  devtool.send,
+  devtool.subscribe
 );
 
 require('basis.app').create(
