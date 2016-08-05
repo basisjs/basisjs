0c2bd7ba019d27013bf83d5fe24304e24b8a607d
Roman Dvornov <rdvornov@gmail.com>
2016-08-05 20:44:45 +0300

    devpanel: preparations for development mode in devtool

--- src/devpanel/basisjs-tools-sync.js
+++ src/devpanel/basisjs-tools-sync.js
@@ -176,10 +176,15 @@ basis.ready(function(){
   {
     var remoteApi = basisjsTools.initRemoteDevtoolAPI({
       getInspectorUI: function(dev, callback){
+        // if (dev)
+        //   return callback(null, 'url', basis.path.origin + basis.path.resolve(__dirname, 'standalone.html'));
+
         basisjsTools.getBundle(dev ? asset('./standalone.html') : {
           build: asset('../../dist/devtool.js'),
           filename: asset('./standalone.html')
-        }, callback);
+        }, function(err, script){
+          callback(err, 'script', script);
+        });
       }
     });
 
