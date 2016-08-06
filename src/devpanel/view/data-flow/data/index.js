47f2ddc651441d6063e2d3afd0870dd6fbfba1ae
Roman Dvornov <rdvornov@gmail.com>
2016-08-07 01:01:50 +0300

    devpanel: update data flow by timer

--- src/devpanel/view/data-flow/data/index.js
+++ src/devpanel/view/data-flow/data/index.js
@@ -2,13 +2,25 @@ var inspectBasis = require('devpanel').inspectBasis;
 var Value = require('basis.data').Value;
 var createTreeBuilder = require('./build-tree.js');
 
-var selectedObject = new Value();
+var input = new Value();
+var selectedObject = new basis.Token();
+
 var tree = selectedObject
   .as(createTreeBuilder({
     sandbox: inspectBasis
-  }));
+  }))
+  // pass through stringify/parse to avoid notification about the same structure
+  .as(JSON.stringify)
+  .as(JSON.parse);
+
+// rebuild graph by timer
+input.link(selectedObject, selectedObject.set);
+setInterval(function(){
+  if (selectedObject.value)
+    selectedObject.apply();
+}, 100);
 
 module.exports = {
-  input: selectedObject,
-  output: tree
+  input: input,
+  output: Value.from(tree)
 };
