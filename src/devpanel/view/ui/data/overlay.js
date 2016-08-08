c630df3f381535cc6c4c031b2eaf1b94b3b372d4
Roman Dvornov <rdvornov@gmail.com>
2016-08-08 13:10:07 +0300

    devpanel: improve UI component overlay

--- src/devpanel/view/ui/data/overlay.js
+++ src/devpanel/view/ui/data/overlay.js
@@ -1,7 +1,9 @@
 var Value = require('basis.data').Value;
+var Expression = require('basis.data.value').Expression;
 var getBoundingRect = require('basis.layout').getBoundingRect;
 var Node = require('basis.ui').Node;
 var map = require('./index.js').map;
+var output = require('./index.js').output;
 var hoverById = new Value();
 var overlay = new Node({
   template: resource('./overlay.tmpl'),
@@ -13,13 +15,13 @@ var overlay = new Node({
   }
 });
 
-hoverById
-  .as(function(id){
-    return map[id] ? map[id].instance : null;
-  })
-  .link(null, function(instance){
-    var element = instance && instance.tmpl.element;
-
+// output uses as mediator to recalculate element
+new Expression(hoverById, output, function(id){
+  var instance = map[id] && map[id].instance;
+  var element = instance && instance.tmpl && instance.tmpl.element;
+  return element || null;
+})
+  .link(null, function(element){
     if (!element)
     {
       if (overlay.element.parentNode)
