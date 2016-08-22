c20be667a425a67ef4c5a421c90be837c83b4a4c
Ivan Strelkov <iv-strelkov@yandex.ru>
2016-08-22 13:51:37 +0300

    Added type.int

--- src/basis/type.js
+++ src/basis/type.js
@@ -77,7 +77,7 @@ function numberTransform(defaultValue) {
 function nullableNumberTransform(defaultValue) {
   if (defaultValue !== null && !isNumber(defaultValue))
   {
-    /** @cut */ basis.dev.warn('type.number.nullable.default expected number as default value but got ' + defaultValue + '. Falling back to type.number.nullable');
+    /** @cut */ basis.dev.warn('type.number.nullable.default expected number or null as default value but got ' + defaultValue + '. Falling back to type.number.nullable');
     return number.nullable;
   }
 
@@ -104,8 +104,65 @@ number.default = numberTransform;
 number.nullable = nullableNumberTransform(null);
 number.nullable.default = nullableNumberTransform;
 
+function Int(value) {
+  return parseInt(value, 10);
+}
+
+function intTransform(defaultValue) {
+  if (!isNumber(defaultValue))
+  {
+    /** @cut */ basis.dev.warn('type.int.default expected number as default value but got ' + defaultValue + '. Falling back to type.int');
+    return int;
+  }
+
+  defaultValue = Int(defaultValue);
+
+  return function(value, oldValue){
+    if (isNumber(value))
+      return Int(value);
+
+    if (value === DEFAULT_VALUE)
+      return defaultValue;
+
+    /** @cut */ basis.dev.warn('type.int expected int but got ' + value);
+
+    return oldValue;
+  };
+}
+
+function nullableIntTransform(defaultValue) {
+  if (defaultValue !== null && !isNumber(defaultValue))
+  {
+    /** @cut */ basis.dev.warn('type.int.nullable.default expected number or null as default value but got ' + defaultValue + '. Falling back to type.int.nullable');
+    return int.nullable;
+  }
+
+  defaultValue = defaultValue === null ? null : Int(defaultValue);
+
+  return function(value, oldValue){
+    if (isNumber(value))
+      return Int(value);
+
+    if (value === null)
+      return null;
+
+    if (value === DEFAULT_VALUE)
+      return defaultValue;
+
+    /** @cut */ basis.dev.warn('type.int expected int or null but got ' + value);
+
+    return oldValue;
+  };
+}
+
+var int = intTransform(0);
+int.default = intTransform;
+int.nullable = nullableIntTransform(null);
+int.nullable.default = nullableIntTransform;
+
 module.exports = {
   string: string,
   number: number,
+  int: int,
   DEFAULT_VALUE: DEFAULT_VALUE
 };
