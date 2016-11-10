606219f1af2aa607300b0810c82897390f2b60e8
Sergey Melyukov <smelyukov@avito.ru>
2016-11-10 11:59:27 +0300

    devpanel: fixed memory leak

--- src/devpanel/view/template-info/globalElements.js
+++ src/devpanel/view/template-info/globalElements.js
@@ -2,15 +2,35 @@ var MAX_HISTORY_SIZE = 4;
 var PREFIX = '$b';
 var history = [];
 
+var ELEMENT_DESTROY_HANDLER = {
+  destroy: function(){
+    for (var position = 0; position < history.length; position++){
+      if (history[position] == this){
+        global[PREFIX + position] = history[position] = null;
+      }
+    }
+  }
+};
+
 function initGlobalElements(data){
   data.output.addHandler({
     change: function(){
-      if (!this.value.object) {
+      var addCandidate = this.value.object;
+
+      if (!addCandidate || history[0] == addCandidate){
         return;
       }
 
-      history.unshift(this.value.object);
-      history.length = MAX_HISTORY_SIZE;
+      history.unshift(addCandidate);
+      addCandidate.addHandler(ELEMENT_DESTROY_HANDLER);
+
+      if (history.length > MAX_HISTORY_SIZE){
+        var deleteCandidate = history.pop();
+
+        if (deleteCandidate){
+          deleteCandidate.removeHandler(ELEMENT_DESTROY_HANDLER);
+        }
+      }
 
       history.forEach(function(element, position){
         global[PREFIX + position] = element;
