3a5045153499a4fca098dd5483b47ccf48bd5e50
Ivan Strelkov <iv-strelkov@yandex.ru>
2017-02-19 21:22:47 +0300

    basis.router: implemented route parsing to AST

--- src/basis/router/ast.js
+++ src/basis/router/ast.js
@@ -1,3 +1,11 @@
+var TYPE = {
+  PLAIN_PARAM: 'PLAIN_PARAM',
+  ANY_PARAM: 'ANY_PARAM',
+  WORD: 'WORD',
+  GROUP: 'GROUP',
+  GROUP_OPTION: 'GROUP_OPTION'
+};
+
 function parsePath(route){
   var value = String(route || '');
   var params = [];
@@ -6,9 +14,55 @@ function parsePath(route){
     return value.substr(offset).match(/^\w+/);
   }
 
-  function parse(offset, stopChar){
+  function getOption(i) {
+    return parse(i + 1, ')', '|');
+  }
+
+  function getGroup(i) {
+    var res;
+    var result = '';
+    var options = [];
+
+    while (res = getOption(i)) {
+      options.push({
+        type: TYPE.GROUP_OPTION,
+        children: res.AST
+      });
+      i = res.offset;
+      result += res.result;
+
+      if (res.stoppedAt == ')') {
+        return {
+          type: TYPE.GROUP,
+          options: options,
+          result: result,
+          offset: i
+        };
+      }
+      else
+      {
+        result += '|';
+      }
+
+    }
+    return null;
+  }
+
+  function parse(offset, stopChar, anotherStopChar){
     var result = '';
     var res;
+    var curWord = '';
+    var AST = [];
+
+    function putCurrentWord() {
+      if (curWord) {
+        AST.push({
+          type: TYPE.WORD,
+          name: curWord
+        });
+        curWord = '';
+      }
+    }
 
     for (var i = offset; i < value.length; i++)
     {
@@ -16,38 +70,56 @@ function parsePath(route){
       switch (c)
       {
         case stopChar:
+        case anotherStopChar:
+          putCurrentWord();
           return {
             result: result,
-            offset: i
+            offset: i,
+            stoppedAt: c,
+            AST: AST
           };
 
         case '\\':
-          result += '\\' + value.charAt(++i);
+          var nextChar = value.charAt(++i);
+          result += '\\' + nextChar;
+          curWord += nextChar;
           break;
 
         case '|':  // allow | inside braces
           result += stopChar != ')' ? '\\|' : '|';
+          curWord += '|';
           break;
 
         case '(':  // optional: (something) -> (?:something)?
-          if (res = parse(i + 1, ')'))
+          if (res = getGroup(i))
           {
             i = res.offset;
             result += '(?:' + res.result + ')?';
+            putCurrentWord();
+            AST.push({
+              type: TYPE.GROUP,
+              options: res.options
+            });
           }
           else
           {
             result += '\\(';
+            curWord += '(';
           }
 
           break;
 
         case ':':  // named:   :name -> ([^/]+)
+          putCurrentWord();
           if (res = findWord(i + 1))
           {
             i += res[0].length;
             result += '([^\/]+)';
             params.push(res[0]);
+            AST.push({
+              type: TYPE.PLAIN_PARAM,
+              name: res[0]
+            });
           }
           else
           {
@@ -57,11 +129,16 @@ function parsePath(route){
           break;
 
         case '*':  // splat:   *name -> (.*?)
+          putCurrentWord();
           if (res = findWord(i + 1))
           {
             i += res[0].length;
             result += '(.*?)';
             params.push(res[0]);
+            AST.push({
+              type: TYPE.ANY_PARAM,
+              name: res[0]
+            });
           }
           else
           {
@@ -72,20 +149,28 @@ function parsePath(route){
 
         default:
           result += basis.string.forRegExp(c);
+          curWord += c;
       }
     }
 
-    return stopChar ? null : result;
+    putCurrentWord();
+
+    return stopChar ? null : {
+      regexpStr: result,
+      AST: AST
+    };
   }
 
-  var regexp = new RegExp('^' + parse(0) + '$', 'i');
+  var parsingResult = parse(0);
+  var regexp = new RegExp('^' + parsingResult.regexpStr + '$', 'i');
   regexp.params = params;
   return {
     regexp: regexp,
-    AST: null
+    AST: parsingResult.AST
   };
 }
 
 module.exports = {
-  parsePath: parsePath
+  parsePath: parsePath,
+  TYPE: TYPE
 };
--- test/spec/router/ast.js
+++ test/spec/router/ast.js
@@ -1,10 +1,189 @@
+// PLAIN_PARAM(name)
+// ANY_PARAM(name)
+// WORD(name)
+// GROUP(options)
+
 module.exports = {
-  name: 'parsePath',
+  name: 'parsePath AST',
   init: function(){
-    var parsePath = basis.require('basis.router.ast').parsePath;
+    var AST = basis.require('basis.router.ast');
+    var parsePath = AST.parsePath;
+
+    function plain(name) {
+      return { type: AST.TYPE.PLAIN_PARAM, name: name };
+    }
+    function any(name) {
+      return { type: AST.TYPE.ANY_PARAM, name: name };
+    }
+    function word(name) {
+      return { type: AST.TYPE.WORD, name: name };
+    }
+    function group() {
+      return { type: AST.TYPE.GROUP, options: basis.array.from(arguments) };
+    }
+    function option() {
+      return { type: AST.TYPE.GROUP_OPTION, children: basis.array.from(arguments) };
+    }
+
+    function ast(path) {
+      return JSON.stringify(parsePath(path).AST);
+    }
+    function str(obj) {
+      return JSON.stringify(obj);
+    }
   },
-  test: function(){
-    // var ESCAPE_FOR_REGEXP = /([\/\\\(\)\[\]\?\{\}\|\*\+\-\.\^\$])/g;
-    // escaped chars: /\()[]?{}|*+-.^$
-  }
+  test: [
+    {
+      name: 'simple case',
+      test: function(){
+        var actual = ast('foo/:bar/*baz');
+        var expected = str([
+          word('foo/'),
+          plain('bar'),
+          word('/'),
+          any('baz')
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'word with extra chars',
+      test: function(){
+        var actual = ast('/[]?{}/:bar/|+-.^');
+        var expected = str([
+          word('/[]?{}/'),
+          plain('bar'),
+          word('/|+-.^')
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'group with one child',
+      test: function(){
+        var actual = ast('page/sub(/)(:rest)(complex/*sub)');
+        var expected = str([
+          word('page/sub'),
+          group(
+            option(
+              word('/')
+            )
+          ),
+          group(
+            option(
+              plain('rest')
+            )
+          ),
+          group(
+            option(
+              word('complex/'),
+              any('sub')
+            )
+          )
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'word with non-closing bracket',
+      test: function(){
+        var actual = ast('page/sub)(');
+        var expected = str([
+          word('page/sub)(')
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'group with multiple options',
+      test: function(){
+        var actual = ast('page/(:foo|bar|*baz)/end');
+        var expected = str([
+          word('page/'),
+          group(
+            option(
+              plain('foo')
+            ),
+            option(
+              word('bar')
+            ),
+            option(
+              any('baz')
+            )
+          ),
+          word('/end')
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'recursive group',
+      test: function(){
+        var actual = ast('page/(:foo|(bar|:spam(/))|*baz)/end');
+        var expected = str([
+          word('page/'),
+          group(
+            option(
+              plain('foo')
+            ),
+            option(
+              group(
+                option(
+                  word('bar')
+                ),
+                option(
+                  plain('spam'),
+                  group(
+                    option(
+                      word('/')
+                    )
+                  )
+                )
+              )
+            ),
+            option(
+              any('baz')
+            )
+          ),
+          word('/end')
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'escaping',
+      test: function(){
+        var actual = ast('page\\/\\(\\:foo\\|bar\\|\\*baz\\)\\/\\end\\\\');
+        var expected = str([
+          word('page/(:foo|bar|*baz)/end\\')
+        ]);
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'regexp - groups and params',
+      test: function(){
+        var actual = parsePath('page/(:foo|(bar|:spam(/))|*baz)/end').regexp;
+        var expected = /^page\/(?:([^\/]+)|(?:bar|([^\/]+)(?:\/)?)?|(.*?))?\/end$/i.toString();
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'regexp - escaping',
+      test: function(){
+        // page\/\(\:foo\|bar\|\*baz\)\/\end\\
+        var actual = parsePath('page\\/\\(\\:foo\\|bar\\|\\*baz\\)\\/\\end\\\\').regexp;
+        var expected = /^page\/\(\:foo\|bar\|\*baz\)\/\end\\$/i.toString();
+        assert(actual == expected);
+      }
+    },
+    {
+      name: 'regexp - symbols',
+      test: function(){
+        var actual = parsePath('/[]?{}/:bar/|+-.^').regexp;
+        var expected = /^\/\[\]\?\{\}\/([^\/]+)\/\|\+\-\.\^$/i.toString();
+        assert(actual == expected);
+      }
+    }
+  ]
 };
