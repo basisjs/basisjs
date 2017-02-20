7fd1ddc3487d59e160062b6a5b93ebc9ccb926e1
Ivan Strelkov <iv-strelkov@yandex.ru>
2017-02-20 16:28:19 +0300

    assign: renamed object-assign to assign

--- src/basis/utils/json-parser.js
+++ src/basis/utils/json-parser.js
@@ -3,7 +3,7 @@
 
 'use strict';
 
-require('./object-assign.js');
+require('./assign.js');
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
