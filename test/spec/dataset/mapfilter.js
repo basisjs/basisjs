b10fcffba9ac5d786f4bb77308357d2e00428c4d
Sergey Melyukov <s.melukov@gmail.com>
2017-03-02 17:27:06 +0300

    basis.data: use safe data.isEqual() function for comparation objects in basis.data.dataset.MapFilter (#148)

--- test/spec/dataset/mapfilter.js
+++ test/spec/dataset/mapfilter.js
@@ -4,6 +4,22 @@ module.exports = {
     var MapFilter = basis.require('basis.data.dataset').MapFilter;
     var Dataset = basis.require('basis.data').Dataset;
     var DataObject = basis.require('basis.data').Object;
+    var devWrap = basis.require('basis.data').devWrap;
+    var helpers = basis.require('./helpers/dataset.js');
+    var range = helpers.range;
+
+    function generateDataset(elements, map) {
+      map = map || basis.fn.$self;
+
+      return new Dataset({
+        syncAction: function(){
+          this.set(range(1, elements)
+            .map(function(value){
+              return map(new DataObject({ data: { value: value } }));
+            }));
+        }
+      });
+    }
   },
   test: [
     {
@@ -13,7 +29,7 @@ module.exports = {
 
         function generateDataset() {
           return new Dataset({
-            syncAction: function(data){
+            syncAction: function(){
               this.set([
                 new DataObject({ data: { views: idx++ } })
               ]);
@@ -32,6 +48,92 @@ module.exports = {
         result.setSource(generateDataset());
 
         assert([2], result.getValues('data.views'));
+      }
+    },
+    {
+      name: 'mapping',
+      test: function(){
+        var result = new MapFilter({
+          active: true,
+          map: function(obj){
+            return obj.data.sub;
+          }
+        });
+
+        result.setSource(generateDataset(5, function(value){
+          return new DataObject({ data: { sub: value } });
+        }));
+        assert([1, 2, 3, 4, 5], result.getValues('data.value'));
+      }
+    },
+    {
+      name: 'filtering',
+      test: function(){
+        var result = new MapFilter({
+          active: true,
+          map: function(obj){
+            return obj.data.sub;
+          },
+          filter: function(obj){
+            return obj.data.value <= 2; // inverted logic - not like Array#filter
+          }
+        });
+
+        result.setSource(generateDataset(5, function(value){
+          return new DataObject({ data: { sub: value } });
+        }));
+        assert([3, 4, 5], result.getValues('data.value'));
+
+        result.setFilter(function(obj){
+          return obj.data.value <= 3; // inverted logic - not like Array#filter
+        });
+        assert([4, 5], result.getValues('data.value'));
+      }
+    },
+    {
+      name: 'filtering with objects update',
+      test: function(){
+        var result = new MapFilter({
+          active: true,
+          map: function(obj){
+            return obj.data.sub;
+          },
+          filter: function(obj){
+            return obj.data.value <= 2; // inverted logic - not like Array#filter
+          }
+        });
+
+        result.setSource(generateDataset(5, function(value){
+          return new DataObject({ data: { sub: value } });
+        }));
+        assert([3, 4, 5], result.getValues('data.value'));
+
+        result.source.getItems()[2].data.sub.update({ value: 10 });
+        assert([10, 4, 5], result.getValues('data.value'));
+      }
+    },
+    {
+      name: 'filtering proxy objects',
+      test: function(){
+        var elementToUpdate;
+        var result = new MapFilter({
+          active: true,
+          map: function(obj){
+            return obj.data.sub;
+          },
+          filter: function(obj){
+            return obj.data.value <= 2; // inverted logic - not like Array#filter
+          }
+        });
+
+        result.setSource(generateDataset(5, function(value){
+          return new DataObject({ data: { sub: value } });
+        }), true);
+        assert([3, 4, 5], result.getValues('data.value'));
+
+        elementToUpdate = result.source.getItems()[2];
+        elementToUpdate.update({ sub: devWrap(elementToUpdate.data.sub) });
+        assert([3, 4, 5], result.getValues('data.value'));
       }
     }
   ]
