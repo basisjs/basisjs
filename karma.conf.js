8f07edbb12d24ebb928720aa692b22411f92e8fe
Ivan Strelkov <iv-strelkov@yandex.ru>
2017-06-22 14:48:04 +0300

    Use Chrome Headless for unit testing

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config){
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['ChromeHeadless'],
 
 
     // Continuous Integration mode
